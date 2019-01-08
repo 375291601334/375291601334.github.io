416a022a3ffa78f47f34459a69422f2bac2c57d7
Anastasiya <33834629+375291601334@users.noreply.github.com>
2019-01-08 12:50:15 +0300

    Update keyPressHandlers.js

--- src/playWithKeyboard/keyPressHandlers.js
+++ src/playWithKeyboard/keyPressHandlers.js
@@ -27,3 +27,41 @@ export function pressEnterAndEscHandler(func) {
     }
   };
 }
+
+export function (funcOnEnter, className) {
+  let focusIndex = 0;
+  let focusedElement = document.getElementsByClassName(`${className}`)[focusIndex];
+  focusedElement.focus();
+  prevPressKeyFunc = document.onkeydown;
+  
+  document.onkeydown = function pressKeyFunc(evt) {
+    const event = evt || window.event;
+    switch (event.keyCode) {
+      case LEFT_KEYCODE:
+      case DOWN_KEYCODE:
+        focusIndex -= 1;
+        if (focusIndex === -1) focusIndex = 2;
+        focusedElement = document.getElementsByClassName(`${className}`)[focusIndex];
+        focusedElement.focus();
+        break;
+      case UP_KEYCODE:
+      case RIGHT_KEYCODE:
+        focusIndex += 1;
+        if (focusIndex === 3) focusIndex = 0;
+        focusedElement = document.getElementsByClassName(`${className}`)[focusIndex];
+        focusedElement.focus();
+        break;
+      case ENTER_KEYCODE:
+        funcOnEnter();
+        break;
+      case ESC_KEYCODE:
+        document.onkeydown = prevPressKeyFunc;
+        if (document.getElementsByClassName('modal-window')[0] !== 'undefined') {
+          closeModalWindow();
+        }
+        break;
+      default:
+        break;
+    }
+  }
+}
