5f219ad9321580c15b2ed12449280f68ae8f9b9b
Anastasiya <33834629+375291601334@users.noreply.github.com>
2019-01-08 12:08:33 +0300

    Update englishTranslate.js

--- src/tasks/englishTranslate/englishTranslate.js
+++ src/tasks/englishTranslate/englishTranslate.js
@@ -1,7 +1,9 @@
-import { clearModalWindow, createTaskText, createTaskInput, createTaskButton, randNum, makeSpell } from '../tasksModalWindowElements';
+import { createTaskText, createTaskInput, createTaskButton, randNum, makeSpell } from '../tasksModalWindowElements';
 import isBlank from '../validation';
 import wordsArr from './dictionary';
 import { showSpellWindow } from '../../createSpellWindow/createSpellWindow';
+import { pressEnterHandler } from '../../playWithKeyboard/keyPressHandlers';
+import { clearElement } from '../../modalWindow/modalWindow';
 
 let index;
 let word;
@@ -12,13 +14,7 @@ function checkAnswer() {
     const rightAns = wordsArr[index][word];
     makeSpell(rightAns.indexOf(userAns) !== -1);
   }
-
-  document.onkeydown = function pressKeyFunc(evt) {
-    const event = evt || window.event;
-    if (event.keyCode === 13) {
-      showSpellWindow();
-    }
-  };
+  pressEnterHandler(showSpellWindow);
 }
 
 export default function mathExample() {
@@ -26,18 +22,11 @@ export default function mathExample() {
   index = randNum(0, wordsArr.length - 1);
   word = Object.keys(wordsArr[index])[0];
   const str = `Переведите слово: ${word}`;
-
-  clearModalWindow(modalWindow);
+  clearElement(modalWindow);
   createTaskText(modalWindow, str);
   createTaskInput(modalWindow);
   const input = document.getElementsByClassName('task-input')[0];
   input.focus();
   createTaskButton(modalWindow, checkAnswer);
-
-  document.onkeydown = function pressKeyFunc(evt) {
-    const event = evt || window.event;
-    if (event.keyCode === 13) {
-      checkAnswer();
-    }
-  };
+  pressEnterHandler(checkAnswer);
 }
