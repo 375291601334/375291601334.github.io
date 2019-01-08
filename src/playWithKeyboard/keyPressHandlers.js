7040f290d5b44820935fb5c0423da7f4c0a0ab8c
Anastasiya <33834629+375291601334@users.noreply.github.com>
2019-01-08 12:13:27 +0300

    Add files via upload

---
+++ src/playWithKeyboard/keyPressHandlers.js
@@ -0,0 +1,29 @@
+import { closeModalWindow } from '../modalWindow/modalWindow';
+
+const ENTER_KEYCODE = 13;
+const ESC_KEYCODE = 27;
+const prevPressKeyFunc;
+
+export function pressEnterHandler(func) {
+  document.onkeydown = function pressKeyFunc(evt) {
+    const event = evt || window.event;
+	if (event.keyCode === ENTER_KEYCODE) {
+	  func();
+    }
+  };
+}
+
+export function pressEnterAndEscHandler(func) {
+  prevPressKeyFunc = document.onkeydown;
+  document.onkeydown = function pressKeyFunc(evt) {
+    const event = evt || window.event;
+    if (event.keyCode === ENTER_KEYCODE) {
+      func();
+    } else if (event.keyCode === ESC_KEYCODE) {
+      document.onkeydown = prevPressKeyFunc;
+      if (document.getElementsByClassName('modal-window')[0] !== 'undefined') {
+        closeModalWindow();
+      }
+    }
+  };
+ }
